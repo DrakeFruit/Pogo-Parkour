e48c80e725e6a7f385aaa62a09836769e1fb125c
DrakeFruit <94070205+DrakeFruit@users.noreply.github.com>
2024-09-01 21:13:44 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pogo Parkour
+ A pogostick parkour game inspired by pogostuck and unicycle frenzy
---
+++ code/Player/Controller/PogoController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+using Sandbox;
+
+public sealed class PogoController : Component
+{
+	[Property] public float BaseJumpForce = 1000;
+	[Property] public float MaxJumpForce { get; set; } = 15f;
+	[Property] public SphereCollider Contact { get; set; }
+	[RequireComponent] public Rigidbody rigidbody { get; set; }
+	public float JumpForce { get; set; }
+	public bool IsGrounded = false;
+	public bool IsJumping = false;
+	public TimeSince TimeHeld = 0f;
+	protected override void OnFixedUpdate()
+	{
+		//Grounded check
+		SceneTraceResult groundTrace = Scene.Trace.Body( Contact.Rigidbody.PhysicsBody, Transform.Position + Vector3.Down * 5)
+			.IgnoreGameObjectHierarchy( GameObject )
+			.Run();
+
+		if( groundTrace.Hit ) IsGrounded = true; else IsGrounded = false;
+		if( IsGrounded ) 
+		{
+			rigidbody.PhysicsBody.Velocity = 0;
+			rigidbody.Gravity = false;
+		} else
+		{
+			rigidbody.Gravity = true;
+		}
+
+		//Jumping
+		if( Input.Down( "Jump" ) )
+		{
+			if( !IsJumping ) TimeHeld = 0f;
+			IsJumping = true;
+			
+			JumpForce = BaseJumpForce + TimeHeld * 600;
+			JumpForce = float.Clamp(JumpForce, BaseJumpForce, MaxJumpForce * 100);
+			Log.Info(JumpForce);
+		}
+		if( Input.Released( "Jump" ) )
+		{
+			if(IsJumping) IsJumping = false;
+			TimeHeld = 0f;
+			Log.Info("jump");
+			if( IsGrounded ) rigidbody.ApplyImpulse( Transform.Rotation.Up * JumpForce * rigidbody.PhysicsBody.Mass );
+		}
+
+		Scene.Camera.Transform.Position = Transform.Position + Vector3.Up * 200 + Vector3.Backward * 400;
+		Transform.Rotation *= new Rotation(-Input.AnalogMove.x, -Input.AnalogMove.y, 0, 0) * Scene.Camera.Transform.Rotation / 100; 
+	}
+}
